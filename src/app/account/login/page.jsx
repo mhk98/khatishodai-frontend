4ab74ad47c23d08d1d9933df497ca309404c8289
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2025-09-27 12:34:46 +0600

    blog added

--- src/app/account/login/page.jsx
+++ src/app/account/login/page.jsx
@@ -1,4 +1,38 @@
+// 'use client';
+// import React from 'react';
+
+// import BreadCrumb from '~/components/elements/BreadCrumb';
+// import Login from '~/components/partials/account/Login';
+// import PageContainer from '~/components/layouts/PageContainer';
+// import FooterDefault from '~/components/shared/footers/FooterDefault';
+// import Newletters from '~/components/partials/commons/Newletters';
+
+// export default function LoginPage() {
+//     const breadCrumb = [
+//         {
+//             text: 'Home',
+//             url: '/',
+//         },
+//         {
+//             text: 'Login',
+//         },
+//     ];
+//     return (
+//         <>
+//             <PageContainer footer={<FooterDefault />} title="Login">
+//                 <div className="ps-page--my-account">
+//                     <BreadCrumb breacrumb={breadCrumb} />
+//                     <Login />
+//                 </div>
+//                 <Newletters layout="container" />
+//             </PageContainer>
+//         </>
+//     );
+// }
+
+
 'use client';
+
 import React from 'react';
 
 import BreadCrumb from '~/components/elements/BreadCrumb';
@@ -8,24 +42,18 @@ import FooterDefault from '~/components/shared/footers/FooterDefault';
 import Newletters from '~/components/partials/commons/Newletters';
 
 export default function LoginPage() {
-    const breadCrumb = [
-        {
-            text: 'Home',
-            url: '/',
-        },
-        {
-            text: 'Login',
-        },
+    const breadcrumb = [
+        { text: 'Home', url: '/' },
+        { text: 'Login' },
     ];
+
     return (
-        <>
-            <PageContainer footer={<FooterDefault />} title="Login">
-                <div className="ps-page--my-account">
-                    <BreadCrumb breacrumb={breadCrumb} />
-                    <Login />
-                </div>
-                <Newletters layout="container" />
-            </PageContainer>
-        </>
+        <PageContainer footer={<FooterDefault />} title="Login">
+            <div className="ps-page--my-account">
+                <BreadCrumb breadcrumb={breadcrumb} />
+                <Login />
+            </div>
+            <Newletters layout="container" />
+        </PageContainer>
     );
 }
