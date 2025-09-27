0ae510a170a4bcabdb233ba5e1e441731215b40d
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2025-09-27 16:09:54 +0600

    shops added

--- next.config.js
+++ next.config.js
@@ -1,42 +1,42 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     eslint: {
-//         ignoreDuringBuilds: true,
-//     },
-//     env: {
-//         title: 'Martfury',
-//         titleDescription: 'Multipurpose Marketplace React Ecommerce Template',
-//     },
-//     experimental: {
-//         missingSuspenseWithCSRBailout: false,
-//     },
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: 'https',
-//                 hostname: 'backend.eaconsultancy.info/', // Production hostname
-//                 pathname: '/**', // Allow all paths
-//             },
-//             {
-//                 protocol: 'http',
-//                 hostname: 'localhost', // Local development hostname
-//                 port: '5000', // Port for localhost (if required)
-//                 pathname: '/**', // Allow all paths
-//             },
-//         ],
-//     },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    env: {
+        title: 'Martfury',
+        titleDescription: 'Multipurpose Marketplace React Ecommerce Template',
+    },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
     images: {
-        domains: [
-            'backend.eaconsultancy.info', // আপনার live backend
-            'localhost', // dev server এর জন্য
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'backend.eaconsultancy.info/', // Production hostname
+                pathname: '/**', // Allow all paths
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost', // Local development hostname
+                port: '5000', // Port for localhost (if required)
+                pathname: '/**', // Allow all paths
+            },
         ],
     },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: [
+//             'backend.eaconsultancy.info', // আপনার live backend
+//             'localhost', // dev server এর জন্য
+//         ],
+//     },
+// };
+
+// module.exports = nextConfig;
